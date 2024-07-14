c8353b46bc5ce0217262795ea772bf6351fed6c1
Muddy Hound <furryballa3@gmail.com>
2024-07-14 21:16:19 +0200

    Update yt-to-mp4.py

--- yt-to-mp4.py
+++ yt-to-mp4.py
@@ -1,54 +1,69 @@
-from pytube import YouTube
 import os
-from moviepy.editor import VideoFileClip, AudioFileClip
 import logging
-from proglog import ProgressBarLogger
+import warnings
+from moviepy.editor import VideoFileClip, AudioFileClip
+import yt_dlp as youtube_dl
 
-class CustomLogger(ProgressBarLogger):
-    def callback(self, **changes):
-        pass
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger(__name__)
 
-    def bars_callback(self, bar, attr, value, old_value=None):
-        pass
+warnings.filterwarnings("ignore", category=UserWarning, module='moviepy')
 
-def download_youtube_video(url, output_path='.', resolution='1080p'):
-    
-    yt = YouTube(url)
+def download_youtube_video(url, output_path='.'):
+    class MyLogger(object):
+        def debug(self, msg):
+            pass
 
-    
-    video_stream = yt.streams.filter(res=resolution, mime_type="video/mp4").first()
-    audio_stream = yt.streams.filter(only_audio=True, mime_type="audio/mp4").first()
+        def warning(self, msg):
+            pass
 
-    if not video_stream:
-        raise Exception(f"No video stream found with resolution {resolution}")
+        def error(self, msg):
+            logger.error(msg)
 
-    if not audio_stream:
-        raise Exception("No audio stream found")
+    ydl_opts_video = {
+        'format': 'bestvideo',
+        'outtmpl': os.path.join(output_path, 'video.%(ext)s'),
+        'logger': MyLogger(),
+        'quiet': True
+    }
+    ydl_opts_audio = {
+        'format': 'bestaudio',
+        'outtmpl': os.path.join(output_path, 'audio.%(ext)s'),
+        'logger': MyLogger(),
+        'quiet': True
+    }
 
-    video_path = os.path.join(output_path, "video.mp4")
-    audio_path = os.path.join(output_path, "audio.mp4")
-    output_file = os.path.join(output_path, f"{yt.title}.mp4")
- 
-    print("[/] Downloading video...")
-    video_stream.download(output_path=output_path, filename="video.mp4")
-    print("[/] Downloading audio...")
-    audio_stream.download(output_path=output_path, filename="audio.mp4")
+    # download vid
+    logger.info("[/] Downloading video...")
+    with youtube_dl.YoutubeDL(ydl_opts_video) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        video_ext = info_dict['ext']
 
-    print("[/] Merging video and audio...")
+    # download aoudio
+    logger.info("[/] Downloading audio...")
+    with youtube_dl.YoutubeDL(ydl_opts_audio) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        audio_ext = info_dict['ext']
+        video_title = info_dict['title']
+
+    video_path = os.path.join(output_path, f'video.{video_ext}')
+    audio_path = os.path.join(output_path, f'audio.{audio_ext}')
+    output_file = os.path.join(output_path, f"{video_title}.mp4")
+
+    # merge
+    logger.info("[/] Merging video and audio...")
     video_clip = VideoFileClip(video_path)
     audio_clip = AudioFileClip(audio_path)
     final_clip = video_clip.set_audio(audio_clip)
-    final_clip.write_videofile(output_file, codec='libx264', logger=CustomLogger())
-
+    final_clip.write_videofile(output_file, codec='libx264', logger=None)
     os.remove(video_path)
     os.remove(audio_path)
-    print(f"[+] Downloaded video!")
+    logger.info(f"[+] Downloaded '{video_title}' successfully!")
 
 if __name__ == '__main__':
-    
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    video_url = input("Enter the YouTube url: ")
+    video_url = input("Enter the YouTube video URL: ")
     try:
         download_youtube_video(video_url, current_dir)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
